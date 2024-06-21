49362f1c4c7ec895a262b6bc191287147c47242d
Thorsten Hindermann <hindermath@googlemail.com>
2024-06-22 00:05:48 +0200

    Modification on the UI.

--- SimpleCalc/ContentView.swift
+++ SimpleCalc/ContentView.swift
@@ -9,40 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var result : String = "0"
+    @State private var number1 : Double = 0
+    @State private var number2 : Double = 0
     var body: some View {
         VStack {
             HStack {
-                Spacer()
                 Text(result)
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .lineLimit(1)
                     .padding()
                     .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
-                Spacer()
             }
-            .padding()
-            
-            Spacer()
             
             HStack {
                 Button("7", action: buttonPressed)
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .id(7)
                 Spacer()
                 Button("8", action: buttonPressed)
                     .padding()
-                .buttonStyle(.borderedProminent)
+                    .buttonStyle(.borderedProminent)
                 Spacer()
                 Button("9", action: buttonPressed)
                     .padding()
-                .buttonStyle(.borderedProminent)
+                    .buttonStyle(.borderedProminent)
                 Spacer()
                 Button("+", action: mathOpPressed)
                     .padding()
-                .buttonStyle(.borderedProminent)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.orange)
             }
-            .padding()
+            
             HStack {
                 Button("4", action: buttonPressed)
                     .padding()
@@ -59,9 +58,10 @@ struct ContentView: View {
                 Button("-", action: mathOpPressed)
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .tint(.orange)
                 
             }
-            .padding()
+            
             HStack {
                 Button("1", action: buttonPressed)
                     .padding()
@@ -78,13 +78,15 @@ struct ContentView: View {
                 Button("*", action: mathOpPressed)
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .tint(.orange)
                 
             }
-            .padding()
+            
             HStack {
                 Button("-", action: negatePressed)
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .tint(.purple)
                 Spacer()
                 Button("0", action: buttonPressed)
                     .padding()
@@ -93,19 +95,21 @@ struct ContentView: View {
                 Button("=", action: equalPressed)
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .tint(.red)
                 Spacer()
                 Button("/", action: mathOpPressed)
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .tint(.orange)
                 
             }
-            .padding()
         }
     }
     func buttonPressed() -> Void {}
-    func negatePressed() -> Void {}
+    func negatePressed() -> Void {result = String((Double(result) ?? 0) * -1)}
     func equalPressed() -> Void {}
-    func mathOpPressed() -> Void {}}
+    func mathOpPressed() -> Void {}
+}
 
 #Preview {
     ContentView()
