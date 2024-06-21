fb10a626b933297131368ca313dbc881a1b7bbcb
Thorsten Hindermann <hindermath@googlemail.com>
2024-06-21 19:03:25 +0200

    Initial Commit

---
+++ SimpleCalc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleCalc
+//
+//  Created by Thorsten Hindermann on 21.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleCalc/SimpleCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalcApp.swift
+//  SimpleCalc
+//
+//  Created by Thorsten Hindermann on 21.06.24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
