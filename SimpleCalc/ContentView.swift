e5729f2ae7312983414f0521c1148017530b0652
Thorsten Hindermann <hindermath@googlemail.com>
2024-06-22 19:55:33 +0200

    Adding basic accessibility (a11) and double tap reaction on the Text view and paddings on various HStacks.

--- SimpleCalc/ContentView.swift
+++ SimpleCalc/ContentView.swift
@@ -21,92 +21,162 @@ struct ContentView: View {
                 Text(result)
                     .font(.largeTitle)
                     .fontWeight(.bold)
+                    .multilineTextAlignment(.trailing)
                     .lineLimit(1)
-                    .padding()
                     .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+                    .accessibilityLabel(result)
+                    .accessibilityHint(result)
+                    .help(result)
+                    .onTapGesture(count: 2, perform: {
+                        result = "0"
+                    })
+
             }
+            .padding([.top, .leading, .trailing])
             
             HStack {
                 Button("7", action: {buttonPressed(number:"7")})
                     .padding()
                     .buttonStyle(.borderedProminent)
-                    .id(7)
+                    .accessibilityLabel("7")
+                    .accessibilityHint("7")
+                    .help("7")
                 Spacer()
                 Button("8", action: {buttonPressed(number:"8")})
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .accessibilityLabel("8")
+                    .accessibilityHint("8")
+                    .help("8")
+
                 Spacer()
                 Button("9", action: {buttonPressed(number:"9")})
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .accessibilityLabel("9")
+                    .accessibilityHint("9")
+                    .help("9")
                 Spacer()
                 Button("+", action: {mathOpPressed(mathop:.plus)})
                     .padding()
                     .buttonStyle(.borderedProminent)
                     .tint(.orange)
-            }
+                    .accessibilityLabel("plus")
+                    .accessibilityHint("plus")
+                    .help("plus")
+
+            }.padding(.horizontal)
             
             HStack {
                 Button("4", action: {buttonPressed(number:"4")})
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .accessibilityLabel("4")
+                    .accessibilityHint("4")
+                    .help("4")
+
                 Spacer()
                 Button("5", action: {buttonPressed(number:"5")})
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .accessibilityLabel("5")
+                    .accessibilityHint("5")
+                    .help("5")
+
                 Spacer()
                 Button("6", action: {buttonPressed(number:"6")})
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .accessibilityLabel("6")
+                    .accessibilityHint("6")
+                    .help("6")
+
                 Spacer()
                 Button("-", action: {mathOpPressed(mathop:.minus)})
                     .padding()
                     .buttonStyle(.borderedProminent)
                     .tint(.orange)
+                    .accessibilityLabel("-")
+                    .accessibilityHint("-")
+                    .help("minus")
+
                 
-            }
+            }.padding(.horizontal)
             
             HStack {
                 Button("1", action: {buttonPressed(number:"1")})
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .accessibilityLabel("1")
+                    .accessibilityHint("1")
+                    .help("1")
+
                 Spacer()
                 Button("2", action: {buttonPressed(number:"2")})
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .accessibilityLabel("2")
+                    .accessibilityHint("2")
+                    .help("2")
+
                 Spacer()
                 Button("3", action: {buttonPressed(number:"3")})
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .accessibilityLabel("3")
+                    .accessibilityHint("3")
+                    .help("3")
+
                 Spacer()
                 Button("*", action: {mathOpPressed(mathop:.multiplication)})
                     .padding()
                     .buttonStyle(.borderedProminent)
                     .tint(.orange)
+                    .accessibilityLabel("*")
+                    .accessibilityHint("*")
+                    .help("muliplication")
+
                 
-            }
+            }.padding(.horizontal)
             
             HStack {
                 Button("-", action: negatePressed)
                     .padding()
                     .buttonStyle(.borderedProminent)
                     .tint(.purple)
+                    .accessibilityLabel("-/+")
+                    .accessibilityHint("-/+")
+                    .help("negation")
+
                 Spacer()
                 Button("0", action: {buttonPressed(number:"0")})
                     .padding()
                     .buttonStyle(.borderedProminent)
+                    .accessibilityLabel("0")
+                    .accessibilityHint("0")
+                    .help("0")
+
                 Spacer()
                 Button("=", action: equalPressed)
                     .padding()
                     .buttonStyle(.borderedProminent)
                     .tint(.red)
+                    .accessibilityLabel("=")
+                    .accessibilityHint("=")
+                    .help("equals")
+
                 Spacer()
                 Button("/", action: {mathOpPressed(mathop:.division)})
                     .padding()
                     .buttonStyle(.borderedProminent)
                     .tint(.orange)
+                    .accessibilityLabel("/")
+                    .accessibilityHint("/")
+                    .help("divison")
+
                 
             }
+            .padding(/*@START_MENU_TOKEN@*/[.leading, .bottom, .trailing]/*@END_MENU_TOKEN@*/)
         }
     }
     func buttonPressed(number : String) -> Void {
