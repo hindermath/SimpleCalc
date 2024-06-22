3ffc26d31b3cdbdc28a4085e1c8177124b2c101e
Thorsten Hindermann <hindermath@googlemail.com>
2024-06-22 15:03:31 +0200

    Implemented complete UI and functionality.

--- SimpleCalc/ContentView.swift
+++ SimpleCalc/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     @State private var result : String = "0"
     @State private var number1 : Double = 0
     @State private var number2 : Double = 0
+    @State private var mathOperation : MathOp = .unknown
+    enum MathOp {
+        case plus, minus, multiplication, division, unknown
+    }
     var body: some View {
         VStack {
             HStack {
@@ -23,39 +27,39 @@ struct ContentView: View {
             }
             
             HStack {
-                Button("7", action: buttonPressed)
+                Button("7", action: {buttonPressed(number:"7")})
                     .padding()
                     .buttonStyle(.borderedProminent)
                     .id(7)
                 Spacer()
-                Button("8", action: buttonPressed)
+                Button("8", action: {buttonPressed(number:"8")})
                     .padding()
                     .buttonStyle(.borderedProminent)
                 Spacer()
-                Button("9", action: buttonPressed)
+                Button("9", action: {buttonPressed(number:"9")})
                     .padding()
                     .buttonStyle(.borderedProminent)
                 Spacer()
-                Button("+", action: mathOpPressed)
+                Button("+", action: {mathOpPressed(mathop:.plus)})
                     .padding()
                     .buttonStyle(.borderedProminent)
                     .tint(.orange)
             }
             
             HStack {
-                Button("4", action: buttonPressed)
+                Button("4", action: {buttonPressed(number:"4")})
                     .padding()
                     .buttonStyle(.borderedProminent)
                 Spacer()
-                Button("5", action: buttonPressed)
+                Button("5", action: {buttonPressed(number:"5")})
                     .padding()
                     .buttonStyle(.borderedProminent)
                 Spacer()
-                Button("6", action: buttonPressed)
+                Button("6", action: {buttonPressed(number:"6")})
                     .padding()
                     .buttonStyle(.borderedProminent)
                 Spacer()
-                Button("-", action: mathOpPressed)
+                Button("-", action: {mathOpPressed(mathop:.minus)})
                     .padding()
                     .buttonStyle(.borderedProminent)
                     .tint(.orange)
@@ -63,19 +67,19 @@ struct ContentView: View {
             }
             
             HStack {
-                Button("1", action: buttonPressed)
+                Button("1", action: {buttonPressed(number:"1")})
                     .padding()
                     .buttonStyle(.borderedProminent)
                 Spacer()
-                Button("2", action: buttonPressed)
+                Button("2", action: {buttonPressed(number:"2")})
                     .padding()
                     .buttonStyle(.borderedProminent)
                 Spacer()
-                Button("3", action: buttonPressed)
+                Button("3", action: {buttonPressed(number:"3")})
                     .padding()
                     .buttonStyle(.borderedProminent)
                 Spacer()
-                Button("*", action: mathOpPressed)
+                Button("*", action: {mathOpPressed(mathop:.multiplication)})
                     .padding()
                     .buttonStyle(.borderedProminent)
                     .tint(.orange)
@@ -88,7 +92,7 @@ struct ContentView: View {
                     .buttonStyle(.borderedProminent)
                     .tint(.purple)
                 Spacer()
-                Button("0", action: buttonPressed)
+                Button("0", action: {buttonPressed(number:"0")})
                     .padding()
                     .buttonStyle(.borderedProminent)
                 Spacer()
@@ -97,7 +101,7 @@ struct ContentView: View {
                     .buttonStyle(.borderedProminent)
                     .tint(.red)
                 Spacer()
-                Button("/", action: mathOpPressed)
+                Button("/", action: {mathOpPressed(mathop:.division)})
                     .padding()
                     .buttonStyle(.borderedProminent)
                     .tint(.orange)
@@ -105,10 +109,55 @@ struct ContentView: View {
             }
         }
     }
-    func buttonPressed() -> Void {}
+    func buttonPressed(number : String) -> Void {
+        if result == "0" {
+            result = number
+        } else {
+            result += number
+        }
+    }
     func negatePressed() -> Void {result = String((Double(result) ?? 0) * -1)}
-    func equalPressed() -> Void {}
-    func mathOpPressed() -> Void {}
+    func equalPressed() -> Void {
+        number2 = Double(result) ?? 0
+        switch mathOperation {
+            case .plus:
+                result = String(number1 + number2)
+            case .minus:
+                result = String(number1 - number2)
+            case .multiplication:
+                result = String(number1 * number2)
+            case .division:
+            if number2 == 0 {
+                let errorString = "Divion by 0!"
+                let attributedString = NSMutableAttributedString(string: errorString)
+                attributedString.addAttribute(.foregroundColor, value: Color.red, range: NSRange(location: 0, length: attributedString.length))
+                result = attributedString.string
+            } else {
+                result = String(number1 / number2)
+            }
+            default:
+            let errorString = "Unkown Error!"
+            let attributedString = NSMutableAttributedString(string: errorString)
+            attributedString.addAttribute(.foregroundColor, value: Color.red, range: NSRange(location: 0, length: attributedString.length))
+            result = attributedString.string
+        }
+    }
+    func mathOpPressed(mathop: MathOp) -> Void {
+        number1 = Double(result) ?? 0
+        result = "0"
+        switch mathop {
+        case .plus:
+            mathOperation = .plus
+        case .minus:
+            mathOperation = .minus
+        case .multiplication:
+            mathOperation = .multiplication
+        case .division:
+            mathOperation = .division
+        default:
+            mathOperation = .unknown
+        }
+    }
 }
 
 #Preview {
