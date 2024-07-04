83ffb12d8424e54a21d0d40d3be4e7733543ca30
Thorsten Hindermann <hindermath@googlemail.com>
2024-07-04 22:10:54 +0200

    Check if settings slider for the waiting time is enabled or not. If enabled the sliders waiting tim eis used otherwise a fixed time of 2 seconds.

--- SimpleCalc/ContentView.swift
+++ SimpleCalc/ContentView.swift
@@ -238,14 +238,14 @@ struct ContentView: View {
                 if number2 == 0 {
                     result = "Divison by 0!"
                     errorState = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + seconds_to_wait, execute: Clear)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + (enable_slider ? seconds_to_wait : 2.0), execute: Clear)
                 } else {
                     result = String(number1 / number2)
                 }
             default:
                 result = "Unkown error!"
                 errorState = true
-            DispatchQueue.main.asyncAfter(deadline: .now() + seconds_to_wait, execute: Clear)
+            DispatchQueue.main.asyncAfter(deadline: .now() + (enable_slider ? seconds_to_wait : 2.0), execute: Clear)
         }
         isFocused = true
     }
